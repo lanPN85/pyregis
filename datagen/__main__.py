edf764c22c751a382b945abebb155dfcd4431aec
Phan Ngoc Lan <phan.ngoclan58@gmail.com>
2017-10-17 22:24:19 +0700

    Datagen module

--- datagen/__main__.py
+++ datagen/__main__.py
@@ -1,2 +1,25 @@
+from argparse import ArgumentParser
+
+from . import names
 
 
+def parse_arguments():
+    parser = ArgumentParser()
+
+    parser.add_argument('-c', '--count', type=int, default=10000, dest='COUNT',
+                        help='The number of students to generate. Defaults to 10000.')
+    parser.add_argument('--test', action='store_true', dest='TEST',
+                        help='Toggles test mode, which only prints out data without affecting any database. '
+                             'Overrides --clean.')
+    parser.add_argument('--clean', action='store_true', dest='CLEAN',
+                        help='Whether to delete all data before generating.')
+
+    return parser.parse_args()
+
+
+def main(args):
+    print('Generating %d students ...' % args.COUNT)
+
+
+if __name__ == '__main__':
+    main(parse_arguments())
---
+++ datagen/names.py
@@ -0,0 +1,55 @@
+LAST_NAMES = {
+    'Phan', 'Nguyễn', 'Lê', 'Trần',
+    'Tống', 'Lại', 'Đỗ', 'Đặng',
+    'Ngô', 'Nghiêm', 'Lương', 'Quách',
+    'Vũ', 'Đinh', 'Phạm', 'Huỳnh',
+    'Hoàng', 'Bùi', 'Võ', 'Hồ', 'Dương',
+    'Đoàn', 'Trịnh', 'Trương', 'Phùng',
+    'Tạ', 'Triệu', 'Chu'
+}
+
+MIDDLE_NAMES = {
+    'Văn', 'Thị', 'Đình', 'Ngọc',
+    'Thành', 'Như', 'Chí', 'Đức',
+    'Thanh', 'Thu', 'Xuân', 'Cẩm',
+    'Hồng', 'Hoàng', 'Đại', 'Đinh',
+    'Anh', 'Hạnh', 'Nhân', 'Kỳ',
+    'Vinh', 'Linh', 'Chi', 'Ly',
+    'Thảo', 'Phương', 'Trinh', 'Lân',
+    'Huy', 'Hồng', 'Nga', 'Thắng',
+    'Cường', 'Dũng', 'Vy', 'Lan',
+    'Lâm', 'Hiếu', 'Mạnh', 'Vân',
+    'Hương', 'Minh', 'Đạt', 'Thịnh',
+    'Vương', 'Long', 'Quang', 'Diệu',
+    'Bảo', 'Trí', 'Trung', 'Quân',
+    'Quỳnh', 'Yến', 'An'
+}
+
+FIRST_NAMES = MIDDLE_NAMES.copy()
+FIRST_NAMES.remove('Thị')
+
+MAJORS = [
+    {'name': 'CNTT-TT'},
+    {'name': 'Tự động hóa'},
+    {'name': 'Điện tử viễn thông'},
+    {'name': 'Kĩ thuật hóa học'},
+    {'name': 'Quản trị kinh doanh'},
+    {'name': 'Quản trị du lịch'},
+    {'name': 'Tiếng Anh thương mại'},
+    {'name': 'Marketing'},
+    {'name': 'Công nghệ sinh học'},
+]
+
+SCHOOLS = [
+    {'name': 'Đại học Bách Khoa Hà Nội'},
+    {'name': 'Đại học Kinh tế Quốc dân'},
+    {'name': 'Đại học Xây dựng'},
+    {'name': 'Đại học Sư phạm'},
+    {'name': 'Đại học Ngoại thương'},
+    {'name': 'Đại học Thương mại'},
+    {'name': 'Đại học Hà Nội'},
+    {'name': 'Đại học Mở'},
+    {'name': 'Đại học Quốc gia Hà Nội'},
+    {'name': 'Học viện Kĩ thuật Quân sự'},
+    {'name': 'Đại học Khoa học Xã hội và Nhân văn'}
+]
